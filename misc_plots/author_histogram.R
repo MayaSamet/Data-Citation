9195c8dcd3fcebed03342c95aa116ef4955c9faf
Maya Samet <mayasamet@gmail.com>
2021-01-27 11:21:53 -0800

    clean up histogram code

--- misc_plots/author_histogram.R
+++ misc_plots/author_histogram.R
@@ -16,14 +16,9 @@ ggplot(adc_author_ds_totals,aes(x=number_of_datasets_authored)) +
   stat_bin(binwidth=1, aes(label=..count..), vjust=-1.5)
 
 
-?ggplot
-
-
 ggplot(data = adc_author_ds_totals, aes(x="stub", y=number_of_datasets_authored)) +
   geom_violin(position = position_nudge(x = .2, y = 0), alpha = .8) +
   geom_point(aes(y=number_of_datasets_authored), position = position_jitter(width = .15), size = .5, alpha = 0.8) + 
   geom_boxplot(width = .1, alpha = 0.5, position = position_nudge(x = -.4, y = 0)) 
   #expand_limits(x = 5.25) 
-
-?geom_
 
